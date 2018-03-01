56b72efea6ffec8820bdcc31e6a3686b4c0ccc04
Michal <m.muszy@gmail.com>
2018-03-01 20:20:44 +0100

    app changed

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var links = document.getElementsByTagName("a");
 
-for (i = 1; i < links.length; i++) {
 
-    links[i].className = "link-" + i;
+var Car = function(maxSpeed, driver) {
+
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function (speed, time) {
+        console.log(speed*time);
+    };
+    this.logDriver = function() {
+        console.log("driver name is " + this.driver);
+    };
+
+
 }
+
+var myCar = new Car(70, "szynka");
+var myCar2 = new Car(20, "wieprz");
+var myCar3 = new Car(45, "krowa");
+var myCar4 = new Car(100, "ciul");
+
+myCar.drive(30,5);
+myCar3.logDriver();
+
+
