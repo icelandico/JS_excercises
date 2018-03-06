aacf41c34b06bfe7ea80a4ce0bd04139c2c64892
Michal <m.muszy@gmail.com>
2018-03-06 19:59:38 +0100

    testing

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
+var myVar = $("#content p:last-child");
 
-function setUpEvents() {
-
-    var content = document.getElementById("content");
-    var button =  document.getElementById("show-more");
-
-    button.onclick = function (){
-
-        if(content.className == "open"){
-            //shrink the box
-            content.className = "";
-            button.innerHTML = "Show more";
-        } else {
-            //expand the box
-            content.className = "open";
-            button.innerHTML = "Show less";
-        }
-
-    };
-
-
-}
-
-
-
-window.onload = function() {
-
-    setUpEvents();
-};
+myVar.mouseenter(function() {
+    alert("YOU ENTERED PARAPGRAPH!");
+});
