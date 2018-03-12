72d96076c07b619579ab742dee2df516d2eb91d1
Michal <m.muszy@gmail.com>
2018-03-12 22:11:58 +0100

    js new

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-var rep= "wha#t. a s_toR-y";
-var rep2 = rep.toLowerCase().replace(/\W|_/g, "");
+function func(str) {
+    var arr = str.split(" ");
+    var arr2 = [];
 
-console.log(rep2);
+    for (i=0; i < arr.length; i++) {
+        arr2.push(arr[i][0].toUpperCase()+(arr[i].slice(1)).toLowerCase());
+
+    }
+    var arr3 = arr2.join(" ");
+    console.log(arr2);
+    console.log(arr3);
+
+
+}
+
+
+
+func("WHATS wRONG with ME");
