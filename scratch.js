28a590bcc7413612ce921d9828fd33f077e31d6b
Michal <m.muszy@gmail.com>
2018-03-13 19:21:50 +0100

    JS Exercise file

--- scratch.js
+++ scratch.js
@@ -1,6 +1,6 @@
 // Creates array that consists of max numbers from arrays
 
-
+/*
 function largestOfFour(arr){
     var newArray = [];
     for (i=0; i < arr.length; i++) {
@@ -14,12 +14,24 @@ function largestOfFour(arr){
 
 largestOfFour([[13,27,18,26], [4,5,1,3],[32,35,37,39], [1000,1001,857,1]]);
 
-/*
-var arr2 = [[13,27,18,26],[4,5,1,3],[32,35,37,39], [1000,1001,857,1]]
-console.log(arr2.length);
-
-
-
-
-
 */
+
+// Checks if the string ('target') is the ending string of phrase ('str')
+
+function confirmEnding(str, target) {
+    var last = str.substr(-(target.length));
+    console.log(last);
+
+    if (target.toLowerCase() == last.toLowerCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
+}
+
+confirmEnding("This is a test string", "ing");
+
+
