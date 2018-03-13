f5feb17057bd09e88b1dc9f2d3e59e877f87f45c
Michal <m.muszy@gmail.com>
2018-03-13 19:26:50 +0100

    JS Exercise

--- scratch.js
+++ scratch.js
@@ -18,20 +18,30 @@ largestOfFour([[13,27,18,26], [4,5,1,3],[32,35,37,39], [1000,1001,857,1]]);
 
 // Checks if the string ('target') is the ending string of phrase ('str')
 
+    function confirmEnding(str, target) {
+        var last = str.substr(-(target.length));
+        console.log(last);
+
+        if (target.toLowerCase() == last.toLowerCase()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    confirmEnding("This is a test string", "ing");
+
+// Shorter version of upper one
+
 function confirmEnding(str, target) {
-    var last = str.substr(-(target.length));
-    console.log(last);
-
-    if (target.toLowerCase() == last.toLowerCase()) {
-        return true;
-    }
-    else {
-        return false;
-    }
-
-
-}
+    return str.substr(-target.length) === target;
+};
 
 confirmEnding("This is a test string", "ing");
+
+//
+
+
 
 
