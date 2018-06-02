54b9613cf4a1290bb12a95eb5018a42898fbf1b8
Michal Muszynski <m.muszy@gmail.com>
2018-06-03 00:07:06 +0200

    Remove duplicates

--- js/Set_2.js
+++ js/Set_2.js
@@ -58,4 +58,18 @@ function missingNums(arr) {
     }
 }
 
-console.log(missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]));
+missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]);
+
+// 6. Remove duplicates from array. Without "SET" method.
+
+function removeDups(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i])) {
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+removeDups(["John", "Taylor", "John", "John"]);
