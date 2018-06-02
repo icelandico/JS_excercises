df2a18740c0857e076118c27bfc673db0c3cc30d
Michal Muszynski <m.muszy@gmail.com>
2018-06-02 23:13:22 +0200

    Maskify string exercise

--- js/Set_2.js
+++ js/Set_2.js
@@ -1,8 +1,19 @@
 // 1. Sort numbers in ascending order
 
-function sortNumsAscending(arr) {
-    return arr !== null ? (arr.sort((a,b) => a-b)) : [];
+// function sortNumsAscending(arr) {
+//     return arr !== null ? (arr.sort((a,b) => a-b)) : [];
+// }
+//
+// sortNumsAscending([10,1,50,4]);
+
+// 2. Maskify the string
+
+function maskify(str) {
+    var strTwo = str.split("");
+    for (var i = 0; i < strTwo.length - 4; i++) {
+        strTwo[i] = '#'
+    }
+    return strTwo.join("")
 }
 
-sortNumsAscending([10,1,50,4]);
-
+maskify("1078398712");
