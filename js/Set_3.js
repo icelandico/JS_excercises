dff206599a67a6726d59a353da9e9eb831fe9674
Michal Muszynski <m.muszy@gmail.com>
2018-06-16 20:08:05 +0200

    Pig Latin

--- js/Set_3.js
+++ js/Set_3.js
@@ -11,3 +11,24 @@ function flipEndChars(str) {
 }
 
 flipEndChars('switcharoo');
+
+// 2. Pig Latin. Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+// Leave punctuation marks untouched.
+
+function pigIt(str){
+    let punctuation = new RegExp(/[.,\/#!$%\^&\*;:]/);
+    str = str.split(" ");
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        if (punctuation.test(str[i])) {
+            result.push(str[i])
+        } else {
+            result.push(str[i].slice(1) + str[i][0] + 'ay')
+        }
+    }
+    return result.join(" ")
+}
+
+
+pigIt("Hello world !");
+
