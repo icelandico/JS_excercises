9c132df7d21c5a6f6ee33f18304764c77899b2b8
Michal Muszynski <m.muszy@gmail.com>
2018-06-19 18:28:57 +0200

    Specific letter count in string

--- js/Set_3.js
+++ js/Set_3.js
@@ -81,3 +81,12 @@ function Magic(str) {
 }
 
 Magic('1 2 2011');
+
+// 7. Create a function called that accepts 2 string arguments and returns an integer of the count of occurrences
+// the 2nd argument is found in the first one.
+
+function strCount(str, letter){
+  return (str.split("").filter(x => x === letter)).length
+}
+
+console.log(strCount('Hello', 'l'));
