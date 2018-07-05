c254c45bf8dcd4398f68a413b64398187f23494a
Michal Muszynski <m.muszy@gmail.com>
2018-07-05 19:51:26 +0200

    Refactored

--- js/Set_3.js
+++ js/Set_3.js
@@ -231,12 +231,6 @@ function scrollingText(text){
 
 scrollingText('codewars');
 
-function likes(names) {
-  return names.join(" and ") + ' like this'
-}
-
-likes(['Jacob', 'Alex']);  // 'Jacob and Alex like this'
-
 // 17. You need to return the highest scoring word as a string
 
 let letterPositions = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16,
