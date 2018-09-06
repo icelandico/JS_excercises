b3128ff8890b1fcc00c3522420ae71aea82dd169
Michal Muszynski <m.muszy@gmail.com>
2018-09-06 15:44:07 +0200

    Replace characters with HTML entities

--- js/Set_4.js
+++ js/Set_4.js
@@ -2,7 +2,7 @@
 
 function translatePigLatin(str) {
   const consonant = new RegExp(/^[^aeiou]+/gi);
-  const reg = str.match(consonant)
+  const reg = str.match(consonant);
   if (!consonant.test(str[0])) {
     return str + 'way'
   } else {
@@ -10,14 +10,31 @@ function translatePigLatin(str) {
   }
 }
 
-translatePigLatin('dalbert')
+translatePigLatin('dalbert');
 
 // 2. Given several arrays, create new array containing only unique elements for all of input arrays.
 
 function uniteUnique(a,...arr) {
   const flattenArray = a.concat(arr.reduce((a, b) => a.concat(b)));
-  return Array.from(new Set(flattenArray))
+  return Array.from(new Set(flattenArray));
 }
 
 uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]);
+
+// 3. Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+  const htmlEntities = new RegExp(/[&<>"]/g);
+  const entities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    "''": "&quot;",
+    '"': "&quot;",
+    "'": "&apos;"
+  }
+  return str.split('').map(item => item.replace(item, entities[item] || item)).join('')
+}
+
+console.log(convertHTML("Schindler's List"));
 
